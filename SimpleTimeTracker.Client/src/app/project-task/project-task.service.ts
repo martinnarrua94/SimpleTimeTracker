69af7f46ddd4948af81d438f6ff4933ed667176c
Martin Arrua <martinnarrua94@gmail.com>
2021-06-29 19:21:57 -0300

    feat: project task effects and service methods added

--- SimpleTimeTracker.Client/src/app/project-task/project-task.service.ts
+++ SimpleTimeTracker.Client/src/app/project-task/project-task.service.ts
@@ -1,24 +1,57 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { IProjectTask } from './project-task';
+import { IProjectTask } from './interfaces/project-task';
+import { IProjectTaskCreate } from './interfaces/project-task-create';
+import { IProjectTaskUpdate } from './interfaces/project-task-update';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectTaskService {
 
-  private APIURL = environment.apiUrl;
+  private APIURL = `${environment.apiUrl}ProjectTasks`;
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<IProjectTask[]> {
-    return this.http.get<IProjectTask[]>(`${this.APIURL}ProjectTasks`).pipe(
+    return this.http.get<IProjectTask[]>(this.APIURL).pipe(
       map((response: IProjectTask[]) => {
         return response;
       })
     )
+  }
+
+  createProjectTask(newProjectTask: IProjectTaskCreate): Observable<IProjectTask> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<IProjectTask>(this.APIURL, newProjectTask, { headers })
+      .pipe(
+        map((response: IProjectTask) => {
+          return response;
+        })
+    );
+  }
+
+  updateProjectTask(projectTask: IProjectTaskUpdate): Observable<IProjectTask>{
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.put<IProjectTask>(`${this.APIURL}/${projectTask.id}`, projectTask, { headers })
+      .pipe(
+        map((response: IProjectTask) => {
+          return response;
+        })
+    );
+  }
+
+  deleteProjectTask(projectTaskId: number): Observable<any>{
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.delete(`${this.APIURL}/${projectTaskId}`, { headers })
+      .pipe(
+        map((response) => {
+          return response;
+        })
+    );
   }
 }
--- SimpleTimeTracker.Client/src/app/project-task/state/project-task.effects.ts
+++ SimpleTimeTracker.Client/src/app/project-task/state/project-task.effects.ts
@@ -2,7 +2,10 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of } from "rxjs";
 import { catchError, map, mergeMap } from "rxjs/operators";
-import { IProjectTask } from "../project-task";
+import { SnackbarOpen } from "src/app/state/shared/snackbar/snackbar.actions";
+import { IProjectTask } from "../interfaces/project-task";
+import { IProjectTaskCreate } from "../interfaces/project-task-create";
+import { IProjectTaskUpdate } from "../interfaces/project-task-update";
 import { ProjectTaskService } from "../project-task.service";
 import * as projectTaskActions from '../state/project-task.actions';
 
@@ -18,6 +21,72 @@ export class ProjectTaskEffects {
                     map((projects: IProjectTask[]) => (new projectTaskActions.LoadSuccess(projects))),
                     catchError(error => of(new projectTaskActions.LoadFail(error)))
             ))
+        )
+    );
+
+    addProjectTask$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.AddProjectTask),
+            map((action: projectTaskActions.AddProjectTask) => action.payload),
+            mergeMap((projectTask: IProjectTaskCreate) =>
+                this.projectTaskService.createProjectTask(projectTask).pipe(
+                    map((createdProjectTask: IProjectTask) => (new projectTaskActions.AddProjectTaskSuccess(createdProjectTask))),
+                    catchError(error => of(new projectTaskActions.AddProjectTaskFail(error)))
+            ))
+        )
+    );
+
+    updateProjectTask$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.UpdateProjectTask),
+            map((action: projectTaskActions.UpdateProjectTask) => action.payload),
+            mergeMap((projectTask: IProjectTaskUpdate) =>
+                this.projectTaskService.updateProjectTask(projectTask).pipe(
+                    map((updatedProjectTask: IProjectTask) => (new projectTaskActions.UpdateProjectTaskSuccess(updatedProjectTask))),
+                    catchError(error => of(new projectTaskActions.UpdateProjectTaskFail(error)))
+            ))
+        )
+    );
+
+    deleteProjectTask$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.DeleteProjectTask),
+            map((action: projectTaskActions.DeleteProjectTask) => action.payload),
+            mergeMap((projectTaskId: number) =>
+                this.projectTaskService.deleteProjectTask(projectTaskId).pipe(
+                    map(() => (new projectTaskActions.DeleteProjectTaskSuccess())),
+                    catchError(error => of(new projectTaskActions.UpdateProjectTaskFail(error)))
+            ))
+        )
+    );
+
+    addProjectTaskSuccess$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.AddProjectTaskSuccess),
+            map(() => new SnackbarOpen({
+                message: 'Project Task created successfully',
+                action: 'Success'
+            })),
+        )
+    );
+
+    updateProjectTaskSuccess$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.UpdateProjectTaskSuccess),
+            map(() => new SnackbarOpen({
+                message: 'Project Task updated successfully',
+                action: 'Success'
+            })),
+        )
+    );
+
+    deleteProjectTaskSuccess$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(projectTaskActions.ProjectTaskActionTypes.DeleteProjectTaskSuccess),
+            map(() => new SnackbarOpen({
+                message: 'Project Task deleted successfully',
+                action: 'Success'
+            })),
         )
     );
 }
